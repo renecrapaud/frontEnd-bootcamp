3592a0b77df98031b87e0aca8d4964a0409a4711
renecrapaud <rene_ab_ing@outlook.com>
2025-09-08 14:59:17 -0600

    Course information 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = (props) =>{
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+          {props.part1} {props.ex1}
+        </p>
+        <p>
+          {props.part2} {props.ex2}
+        </p>
+        <p>
+          {props.part3} {props.ex3}
+        </p>
+    </>
+  )
+}
+
+function Total(props) {
+  return (
+    <>
+      <p>
+        Number of exercises {props.ex1 + props.ex2 + props.ex3}
+      </p>
+    </>
+  )
+}
+
+function App() {
+  
+  const course = 'Half stack application development'
+  const part1 = 'Fundamentals of react'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+        <Header course={course} />
+        <Content 
+          part1={part1} part2={part2} part3={part3}
+          ex1={exercises1} ex2={exercises2} ex3={exercises3}
+        />
+        <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </>
+  )
+}
+
+export default App
