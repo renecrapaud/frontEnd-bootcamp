1576ac7b734df2c1f9f3c26f22b0aebe8fdc4df5
renecrapaud <rene_ab_ing@outlook.com>
2025-09-09 14:49:59 -0600

    Step 2. Unicafe 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,17 +4,55 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const getAverage = (newVal) => {
+    return (good - bad + newVal) / (all + 1)
+  }
+
+  const getNewPositive = (newVal) => {
+    return ((good + newVal) / (all + 1)) * 100
+  }
+
+  const hdlSetGood = () => {
+    let newAll = all + 1
+    setAll(newAll)
+    setGood(good + 1)
+    setAverage(getAverage(1))
+    setPositive(getNewPositive(1))
+  }
+
+  const hdlSetBad = () => {
+    let newAll = all + 1
+    setAll(newAll)
+    setBad(bad + 1)
+    setAverage(getAverage(-1))
+    setPositive(getNewPositive(0))
+  }
+
+  const hdlSetNeutral = () => {
+    let newAll = all + 1
+    setAll(newAll)
+    setNeutral(neutral + 1)
+    setAverage(getAverage(0))
+    setPositive(getNewPositive(0))
+  }
 
   return (
     <>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={hdlSetGood}>good</button>
+      <button onClick={hdlSetNeutral}>neutral</button>
+      <button onClick={hdlSetBad}>bad</button>
       <h2>Statistics</h2>
       <p>good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
     </>
   )
 }
