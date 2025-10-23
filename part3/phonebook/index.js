e830f786d13de1aac95ea3736b64b7d2c40ce6f1
renecrapaud <rene_ab_ing@outlook.com>
2025-10-23 17:31:10 -0600

    3.10 Backend phonebook. Step 10. Deploy backend in render.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 var morgan = require("morgan");
 const cors = require("cors");
+app.use(express.static("dist"));
 
 app.use(express.json());
 app.use(cors());
