3727ff72b98eba7e295e73c871fdc41f4e44c5e1
renecrapaud <rene_ab_ing@outlook.com>
2025-10-23 15:18:10 -0600

    3.9 Backend phonebook. Step 9. Connect with frontend.

--- part2/phonebook/src/services/phoneBookEntry.jsx
+++ part2/phonebook/src/services/phoneBookEntry.jsx
@@ -1,21 +1,21 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = "http://localhost:3001/api/persons";
 
 const getAll = () => {
-    return axios.get(baseUrl)
-}
+  return axios.get(baseUrl);
+};
 
-const create = newObject => {
-    return axios.post(baseUrl, newObject)
-}
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
 
 const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
-}
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
 
 const deleteEntry = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
-}
+  return axios.delete(`${baseUrl}/${id}`);
+};
 
-export default { getAll, create, update, deleteEntry }
+export default { getAll, create, update, deleteEntry };
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,95 +1,100 @@
-const express = require('express')
-const app = express()
-var morgan = require('morgan')
+const express = require("express");
+const app = express();
+var morgan = require("morgan");
+const cors = require("cors");
 
-app.use(express.json())
-morgan.token('namereq', function (req, res) {
-  return req.body && req.body.name ? req.body.name : '-';
+app.use(express.json());
+app.use(cors());
+
+morgan.token("namereq", function (req, res) {
+  return req.body && req.body.name ? req.body.name : "-";
 });
-morgan.token('number', function (req, res) {
-  return req.body && req.body.number ? req.body.number : '-';
+morgan.token("number", function (req, res) {
+  return req.body && req.body.number ? req.body.number : "-";
 });
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms {"name":":namereq","number":":number"}'))
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms {"name":":namereq","number":":number"}',
+  ),
+);
 
 let entries = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
-    response.json(entries)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(entries);
+});
 
-app.get('/info', (request, response) => {
-    const bookEntries = entries.length
-    const dateNow = new Date()
-    const infoTxt = `<p>Phone book has info for ${bookEntries} people <br/> ${dateNow.toString()}</p>`
-    response.send(infoTxt)
-})
+app.get("/info", (request, response) => {
+  const bookEntries = entries.length;
+  const dateNow = new Date();
+  const infoTxt = `<p>Phone book has info for ${bookEntries} people <br/> ${dateNow.toString()}</p>`;
+  response.send(infoTxt);
+});
 
-app.get('/api/persons/:id', (request,response) => {
-    const idtoGet = Number(request.params.id)
-    const personReg = entries.find(reg => reg.id === idtoGet)
-    if(personReg){
-        response.json(personReg)
-    } else {
-        response.status(400).json({ 
-          error: 'content missing'
-        })
-    }
-
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const idtoGet = Number(request.params.id)
-  entries = entries.filter(reg => reg.id !== idtoGet)
-  response.status(204).end()
-})
-
-app.post('/api/persons/', (request, response) => {
-  const body = request.body
-  if(!body?.name || !body?.number){
-    return response.status(400).json({
-      error: 'Missing data in request'
-    })
+app.get("/api/persons/:id", (request, response) => {
+  const idtoGet = Number(request.params.id);
+  const personReg = entries.find((reg) => reg.id === idtoGet);
+  if (personReg) {
+    response.json(personReg);
+  } else {
+    response.status(400).json({
+      error: "content missing",
+    });
   }
-  
-  if(entries.find(reg => reg.name === body.name)){
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const idtoGet = Number(request.params.id);
+  entries = entries.filter((reg) => reg.id !== idtoGet);
+  response.status(204).end();
+});
+
+app.post("/api/persons/", (request, response) => {
+  const body = request.body;
+  if (!body?.name || !body?.number) {
     return response.status(400).json({
-      error: 'Name must be unique'
-    })
+      error: "Missing data in request",
+    });
   }
-  const newId = Math.floor(Math.random() * 50000)
+
+  if (entries.find((reg) => reg.name === body.name)) {
+    return response.status(400).json({
+      error: "Name must be unique",
+    });
+  }
+  const newId = Math.floor(Math.random() * 50000);
 
   const newEntry = {
     id: newId,
     name: body.name,
-    number: body.number
-  }
-  entries = entries.concat(newEntry)
-  response.json(newEntry)
+    number: body.number,
+  };
+  entries = entries.concat(newEntry);
+  response.json(newEntry);
+});
 
-})
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
