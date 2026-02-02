70394ff862c71844eff001250b5107b7bfa04858
renecrapaud <rene_ab_ing@outlook.com>
2026-02-02 14:36:23 -0600

    4.15 Blog list expanssion. Step 3. Adding and getting Users.

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 app.use(express.json())
 
 const { mongoUrl } = require('./utils/config')
@@ -10,6 +11,6 @@ const { mongoUrl } = require('./utils/config')
 app.use(cors())
 mongoose.connect(mongoUrl)
 app.use('/api/blogs', blogsRouter)
-
+app.use('/api/users', usersRouter)
 
 module.exports = app
---
+++ part4/blogList/controllers/users.js
@@ -0,0 +1,26 @@
+const usersRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { username, password, name } = request.body
+  if (!username || !password || !name) {
+    return response.status(400).end()
+  }
+  const saltRounds = 10
+  const passWdHash = await bcrypt.hash(password, saltRounds)
+  const user = new User({
+    username,
+    passWdHash,
+    name
+  })
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
