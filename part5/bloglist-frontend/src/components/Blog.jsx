bf9f6797670975c18e9f257f7bd4c1746eab6e06
renecrapaud <rene_ab_ing@outlook.com>
2026-05-04 20:40:32 -0600

    5.7 BlogList frontend. Step 7. Alternating blog details visibility.

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
 
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const toggleDetails = () => {
+    setVisible(!visible)
+  }
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} <button onClick={toggleDetails}>hide</button> <br />
+        {blog.url} <br />
+        Likes {blog.likes} <button>Like</button> <br />
+        {blog.author}
+
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>View</button>
+      </div>
+    )
+  }
+}
 export default Blog
