eac1750cfe2dbe5e7ca6e24e9f3c75992da2243f
renecrapaud <rene_ab_ing@outlook.com>
2026-05-12 21:48:02 -0600

    5.15 Bloglist frontend tests. Step 3. Check calls to function updateLike

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import blogService from '../services/blogs'
 
 test('renders content', () => {
   const blog = {
@@ -46,4 +47,30 @@ test('Click show details', async () => {
   expect(authorElement).toBeDefined()
   expect(urlElement).toBeDefined()
   expect(likesElement).toBeDefined()
+})
+
+vi.mock('../services/blogs', () => ({
+  default: {
+    updateLike: vi.fn()
+  }
+}))
+
+test('Click twice on like', async () => {
+  const blog = {
+    title: 'The very first entry',
+    url: 'awesome-url.com',
+    author: 'Ghost writer',
+    likes: 1
+  }
+  const mockHandler1 = vi.fn()
+  const { container } = render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+  const buttonView = screen.getByText('View')
+  buttonView.onclick = mockHandler1
+  await user.click(buttonView)
+
+  const buttonLike = screen.getByText('Like')
+  await user.click(buttonLike)
+  await user.click(buttonLike)
+  expect(blogService.updateLike).toHaveBeenCalledTimes(2)
 })
