0ff8f25ee64b378f86c33aacc64e3a404c70f701
Rene Crapaud <rene_ab_ing@outlook.com>
2026-07-08 13:26:56 -0600

    6.8 anecdotes-redux. Step 6. Moving list of anecdotes to a new file.

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteOn } from './reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+
+  const anecdotes = useSelector(state => state.sort(function (a, b) {
+    return b.votes - a.votes
+  }))
+  const dispatch = useDispatch()
+
+  const vote = id => {
+    dispatch(voteOn(id))
+  }
+
+  return (
+    <div>
+      {
+        anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default AnecdoteList
