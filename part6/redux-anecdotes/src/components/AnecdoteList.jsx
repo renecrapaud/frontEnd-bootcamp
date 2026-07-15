8fce7588c7cbb74e2a00f002a2c1417a5a9758e5
Rene Crapaud <rene_ab_ing@outlook.com>
2026-07-14 19:50:10 -0600

    6.9 anecdotes-redux. Step 7. Filtering anecdotes by text.

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,9 +3,14 @@ import { voteOn } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
 
-  const anecdotes = useSelector(state => state.sort(function (a, b) {
-    return b.votes - a.votes
-  }))
+  const anecdotes = useSelector(state => {
+    let filteredAnecdotes = state.filter === '' ? state.anecdotes :
+        state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))
+    return filteredAnecdotes.sort(function (a, b) {
+      return b.votes - a.votes
+    })
+  })
+
   const dispatch = useDispatch()
 
   const vote = id => {
