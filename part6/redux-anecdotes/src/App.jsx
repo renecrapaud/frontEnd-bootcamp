0dae1cd0d0fb48d50bd9a8ebb3b74f50d56bf81b
Rene Crapaud <rene_ab_ing@outlook.com>
2026-07-16 14:27:52 -0600

    6.14 anecdotes-backend. Step 1. Adding json-server, db file, and call to load anecdotes from db.

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import FilterForm from './components/FilterForm'
 import Notification from './components/Notification'
 
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdotesServices from './services/anecdotes'
+
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdotesServices.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
